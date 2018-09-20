89d7a8a50a2490ac224291839976375b5e23fb36
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 18:45:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served";
   } else {
-    return katzDeliLine.shift();
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
