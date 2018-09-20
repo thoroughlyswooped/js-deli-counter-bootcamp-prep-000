3ea5f0ab8662e1e206c02848cae82e54170d81a3
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 18:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function nowServing(katzDeliLine) {
     return katzDeliLine.shift();
   }
 }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty";
+  } else {
+    var lineString = "The line is currently: ";
+    for (var i = 0; i < line.length; i++){
+      lineString += `${i+1} ${line[i]}, `
+    }
+    return lineString;
+  }
+}
