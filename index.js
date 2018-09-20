2d01f98beae27f8d9159830bde57c8b817ce1f99
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 18:45:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(line) {
   if (line.length === 0) {
-    return "The line is currently empty";
+    return "The line is currently empty.";
   } else {
     var lineString = `The line is currently: 1. ${line[0]}`;
     for (var i = 1; i < line.length; i++){
